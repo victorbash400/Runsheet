82ecc42193250f328ce7dac75272f595bcafbbb0
victorbash400 <your-email@example.com>
2025-10-16 16:26:51 +0300

    build 3

--- Runsheet-backend/main.py
+++ Runsheet-backend/main.py
@@ -573,5 +573,5 @@ async def health_check():
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
